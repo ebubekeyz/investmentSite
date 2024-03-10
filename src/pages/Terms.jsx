76629ee74385358620c348a528de6b1c3e5f7759
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-10 11:50:37 +0100

    third

--- src/pages/Terms.jsx
+++ src/pages/Terms.jsx
@@ -6,10 +6,10 @@ const Terms = () => {
       <Title2 title="Terms and Conditions" />
 
       <p>
-        For those of you who are serious about having more, doing more, giving
-        more and being more, success is achievable with some understanding of
-        what to do, some discipline around planning and execution of those plans
-        and belief that you can achieve your desires.
+        For those dedicated to obtaining more, accomplishing more, giving more,
+        and becoming more, success is within reach with a grasp of what needs to
+        be done, disciplined planning, and the belief that you can fulfill your
+        aspirations.
       </p>
 
       <aside style={{ margin: '4rem 0' }}>
@@ -18,14 +18,14 @@ const Terms = () => {
         </h4>
 
         <p>
-          Welcome to the fxidealtrade.co website. By accessing this website, you
-          agree to comply with this binding user agreement between you and
-          fxidealtrade.co, which governs your access and use of the website. If
-          you do not accept these terms and conditions, do not use this website.
-          Your use of this website indicates your full acceptance of this user
-          agreement in its then-current form each time you use the website.
-          fxidealtrade.co reserves the right to change the terms and conditions
-          at any time, without notice.
+          Welcome to the trexholding website. By entering this website, you
+          consent to adhere to this obligatory user agreement between you and
+          trexholding, regulating your access and utilization of the website. If
+          you do not agree with these terms and conditions, refrain from using
+          this website. Your utilization of this website signifies your complete
+          acceptance of this user agreement in its current state each time you
+          access the website. trexholding retains the right to modify the terms
+          and conditions at any time, without prior notice.
         </p>
       </aside>
 
@@ -35,20 +35,20 @@ const Terms = () => {
         </h4>
 
         <p>
-          All content included on this website is the property of
-          fxidealtrade.co or others and is protected by copyright with all
-          rights reserved. You may download or print out a hard copy of
-          individual pages and/or sections of the fxidealtrade.co website,
-          provided that you do not remove any copyright or other proprietary
-          notices. Any downloading or otherwise copying from the fxidealtrade.co
-          website will not transfer title to any software or material to you.
-          You may not reproduce (in whole or in part), transmit (by electronic
-          means or otherwise), modify, link into or use for any public or
-          commercial purpose the fxidealtrade.co website without the prior
-          written permission of fxidealtrade.co. You may not disassemble,
-          deconstruct or otherwise violate the security of all or any portion of
-          this website. At any time and for any reason we may revoke your right
-          to use all or any portion of the website.
+          All content featured on this website is the property of trexholding or
+          other parties and is safeguarded by copyright, with all rights
+          reserved. You are permitted to download or print individual pages
+          and/or sections of the trexholding website, provided that you retain
+          all copyright or other proprietary notices. However, downloading or
+          copying from the trexholding website does not transfer title to any
+          software or material. Reproduction (in whole or in part), transmission
+          (by electronic means or otherwise), modification, linking, or use for
+          any public or commercial purpose of the trexholding website requires
+          the prior written consent of trexholding. Additionally, any attempt to
+          disassemble, deconstruct, or breach the security of the entire or any
+          portion of this website is strictly prohibited. We reserve the right
+          to revoke your access to the website, either in part or in full, at
+          any time and for any reason.
         </p>
       </aside>
 
@@ -58,18 +58,20 @@ const Terms = () => {
         </h4>
 
         <p>
-          Information on this website speaks only as of the date indicated. We
-          make reasonable efforts to provide accurate information, but at times
-          we may not promptly update or correct this website even if we are
-          aware that it is inaccurate, outdated or otherwise inappropriate. You
-          agree that we are not liable for any actions you take or decisions you
-          make in reliance on any information on this website. We make
-          reasonable efforts to avoid technological problems, but at any time
-          this website may have, and/or may cause, technological problems
-          including viruses. We are not liable for any defects, delays or errors
-          in or resulting from your use of this website. We may, but have no
-          obligation to, monitor and record activity on this website for any
-          reason or for no reason.
+          All content featured on this website is the property of trexholding or
+          other parties and is safeguarded by copyright, with all rights
+          reserved. You are permitted to download or print individual pages
+          and/or sections of the trexholding website, provided that you retain
+          all copyright or other proprietary notices. However, downloading or
+          copying from the trexholding website does not transfer title to any
+          software or material. Reproduction (in whole or in part), transmission
+          (by electronic means or otherwise), modification, linking, or use for
+          any public or commercial purpose of the trexholding website requires
+          the prior written consent of trexholding. Additionally, any attempt to
+          disassemble, deconstruct, or breach the security of the entire or any
+          portion of this website is strictly prohibited. We reserve the right
+          to revoke your access to the website, either in part or in full, at
+          any time and for any reason.
         </p>
       </aside>
 
@@ -79,10 +81,10 @@ const Terms = () => {
         </h4>
 
         <p>
-          The information provided on this website is not intended for
+          The information presented on this website is not intended for
           distribution to, or use in, any jurisdiction where such distribution
-          or use would be contrary to law. You are responsible for compliance
-          with applicable local laws and regulations.
+          or use would violate the law. You are accountable for adhering to
+          relevant local laws and regulations.
         </p>
       </aside>
 
@@ -92,9 +94,8 @@ const Terms = () => {
         </h4>
 
         <p>
-          The information and opinions contained on the fxidealtrade.co website
-          are provided without any warranty of any kind, either express or
-          implied.
+          The information and opinions presented on the trexholding website are
+          provided without any warranty, whether express or implied.
         </p>
       </aside>
 
@@ -104,18 +105,18 @@ const Terms = () => {
         </h4>
 
         <p>
-          Neither we nor any of our affiliates, agents or employees is
-          responsible for any damages, liabilities or injury, including but not
-          limited to indirect, incidental, special, punitive or consequential
-          damages however caused arising out your use of (or inability to use)
-          this website or this user agreement. Our liability is limited to the
-          maximum extent permitted by law and is limited even if we have been
-          advised of the possibility of the damages, liability or injury that
-          you suffer or if any remedy you have fails of its essential purpose,
-          including any damages, liabilities or injuries caused by any failure
-          of performance, error, omission, theft, interruption, deletion,
-          defect, delay in operation, communication line failure, or other
-          computer malfunction.
+          Neither we nor any of our affiliates, agents, or employees are
+          responsible for any damages, liabilities, or injuries, including but
+          not limited to indirect, incidental, special, punitive, or
+          consequential damages, however caused, arising from your use of (or
+          inability to use) this website or this user agreement. Our liability
+          is limited to the maximum extent permitted by law, even if we have
+          been advised of the possibility of the damages, liability, or injury
+          you may suffer or if any remedy you have fails in its essential
+          purpose. This includes any damages, liabilities, or injuries caused by
+          failure of performance, error, omission, theft, interruption,
+          deletion, defect, delay in operation, communication line failure, or
+          other computer malfunction.
         </p>
       </aside>
 
@@ -133,9 +134,9 @@ const Terms = () => {
         <p>
           You agree that your use of this website and any disputes relating to
           the website or this user agreement shall be governed in all respects
-          by the laws of the state of Maryland. Any dispute relating to the
-          above shall be brought solely in the state or federal district courts
-          located in Maryland and it is agreed that such courts shall have
+          by the laws of the state of Maryland. Any dispute related to the above
+          shall be brought solely in the state or federal district courts
+          located in Maryland, and it is agreed that such courts shall have
           jurisdiction over the parties to this user agreement and any dispute
           arising hereunder.
         </p>
