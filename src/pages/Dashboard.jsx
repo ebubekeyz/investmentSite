1ae0842b9602a33909c8437e363be8557602a100
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-13 14:33:33 +0100

    twelvth

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,46 @@
+import { useNavigate } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import Navbar2 from '../components/Navbar2';
 import Sidebar from '../components/Sidebar';
+import { useEffect, useState } from 'react';
+import { mainFetch } from '../utils';
 
 const Dashboard = () => {
+  const nav = useNavigate();
+  const [user, setUser] = useState(false);
+  if (user === false) {
+    nav('/login');
+  }
+
+  const fetchUsers = async () => {
+    try {
+      const res = await mainFetch.get('/api/v1/users/showMe', {
+        withCredentials: true,
+      });
+      const users = res.data.user;
+      if (res.status === 200) {
+        setUser(users);
+      }
+
+      return { users };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  console.log(user);
   return (
     <Wrapper>
-      <section className="dashboard">
-        <Navbar2 />
-        <Sidebar />
-      </section>
+      {user && (
+        <section className="dashboard">
+          <Navbar2 />
+          <Sidebar />
+        </section>
+      )}
     </Wrapper>
   );
 };
