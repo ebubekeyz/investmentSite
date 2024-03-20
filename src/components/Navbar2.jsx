3233a7893b2cb8a4bbe6546cbcbf347a599e6558
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-20 10:37:12 +0100

    fourty

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -42,14 +42,12 @@ const Navbar2 = () => {
   };
 
   const logout = async () => {
-    try {
-      await mainFetch.get('/api/v1/auth/logout', { withCredentials: true });
-
-      nav('/login');
-    } catch (error) {
-      console.log(error);
-    }
+    await mainFetch.get('/api/v1/auth/logout');
+    nav('/login');
   };
+  useEffect(() => {
+    logout();
+  }, []);
 
   return (
     <Wrapper>
